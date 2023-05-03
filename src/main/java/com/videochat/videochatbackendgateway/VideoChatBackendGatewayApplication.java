6e88119251bdf507aac0ec2e12826ba8a9bd5838
Gurmigou <yehor.bukhanets@gmail.com>
2023-05-03 23:12:01 +0300

    init

---
+++ src/main/java/com/videochat/videochatbackendgateway/VideoChatBackendGatewayApplication.java
@@ -0,0 +1,12 @@
+package com.videochat.videochatbackendgateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VideoChatBackendGatewayApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(VideoChatBackendGatewayApplication.class, args);
+    }
+}
---
+++ src/test/java/com/videochat/videochatbackendgateway/VideoChatBackendGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.videochat.videochatbackendgateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VideoChatBackendGatewayApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
